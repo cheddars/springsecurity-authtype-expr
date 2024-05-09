5625c757ff4ab9aa62090d10da2545f1aa060339
cheddars <nezahrish@gmail.com>
2024-05-09 23:23:10 +0900

    Basic Auth Testing

---
+++ src/main/java/com/example/secu/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.secu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((req) ->
+            req.requestMatchers("/").permitAll()
+                .requestMatchers("/user.html").hasRole("USER")
+                .requestMatchers("/admin.html").hasRole("ADMIN")
+        )
+        .httpBasic(withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("1234")
+        .roles("USER")
+        .build();
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("1234")
+        .roles("ADMIN", "USER")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+}
